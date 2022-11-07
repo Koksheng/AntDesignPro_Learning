8c8d4786d3790e8a676d9fa1221021c7c72ced79
Koksheng <koksheng0406@gmail.com>
2022-11-07 23:05:01 +0800

    table id sorting

--- src/pages/BasicList/index.tsx
+++ src/pages/BasicList/index.tsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Row, Col, Card, Pagination, Space } from 'antd';
+import { Table, Row, Col, Card, Pagination, Space, Button } from 'antd';
 import { useRequest } from 'umi'; //'@umijs/max';
 import { PageContainer } from '@ant-design/pro-layout';
 import ColumnBuilder from './builder/ColumnBuilder';
 import ActionBuilder from './builder/ActionBuilder';
+import Modal from './component/Modal';
 import styles from './index.less';
 
 const Index = () => {
   const [page, setPage] = useState(1);
   const [per_page, setPerPage] = useState(10);
+  const [sortQuery, setSortQuery] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalUri, setModalUri] = useState('');
   const init = useRequest<{ data: BasicListApi.Data }>(
-    `https://public-api-v2.aspirantzhang.com/api/admins?X-API-KEY=antd&page=${page}&per_page=${per_page}`,
+    `https://public-api-v2.aspirantzhang.com/api/admins?X-API-KEY=antd&page=${page}&per_page=${per_page}${sortQuery}`,
   );
 
   // const init = useRequest(
@@ -19,7 +23,7 @@ const Index = () => {
 
   useEffect(() => {
     init.run();
-  }, [page, per_page]);
+  }, [page, per_page, sortQuery]);
 
   const searchLayout = () => {};
   const beforeTableLayout = () => {
@@ -37,6 +41,14 @@ const Index = () => {
   const paginationChangeHandler = (_page: any, _per_page: any) => {
     setPage(_page);
     setPerPage(_per_page);
+  };
+  const tableChangeHandler = (_: any, __: any, sorter: any) => {
+    if (sorter.order === undefined) {
+      setSortQuery('');
+    } else {
+      const orderBy = sorter.order === 'ascend' ? 'asc' : 'desc';
+      setSortQuery(`&sort=${sorter.field}&order=${orderBy}`);
+    }
   };
   const afterTableLayout = () => {
     return (
@@ -62,17 +74,44 @@ const Index = () => {
 
   return (
     <PageContainer>
+      <Button
+        type="primary"
+        onClick={() => {
+          setModalUri('https://public-api-v2.aspirantzhang.com/api/admins/add?X-API-KEY=antd');
+          setModalVisible(true);
+        }}
+      >
+        Add
+      </Button>
+      <Button
+        type="primary"
+        onClick={() => {
+          setModalUri('https://public-api-v2.aspirantzhang.com/api/admins/206?X-API-KEY=antd');
+          setModalVisible(true);
+        }}
+      >
+        Edit
+      </Button>
       {searchLayout()}
       <Card>
         {beforeTableLayout()}
         <Table
+          rowKey="id"
           dataSource={init?.data?.dataSource}
           columns={ColumnBuilder(init?.data?.layout?.tableColumn)}
           pagination={false}
           loading={init.loading}
+          onChange={tableChangeHandler}
         />
         {afterTableLayout()}
       </Card>
+      <Modal
+        modalVisible={modalVisible}
+        hideModal={() => {
+          setModalVisible(false);
+        }}
+        modalUri={modalUri}
+      />
     </PageContainer>
   );
 };
